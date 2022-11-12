f525e7b8b50fa92913ecfe91b7372774b523791c
Aleksandr677 <hristicenkoaleksandr@MacBook-Air-Hristicenko.local>
2022-11-12 12:07:09 +0300

    feat: add Assets, organize files

---
+++ Slot Machine/Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by Христиченко Александр on 2022-11-11.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
